80ee12b60b4c9e16c242e2b99cce428749f22248
michaels234 <mdsimeone@gmail.com>
2022-11-29 18:33:07 +0900

    didnt commit for long time many changes from now on ill commit more often

--- CameraScript.cs
+++ CameraScript.cs
@@ -4,35 +4,36 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-	public float cameraMaxHeight;
-	public float cameraMinHeight;
-	public float cameraMaxDistance;
-	private float mouseScroll;
+	public float thirdPersonCameraLeft;
+	public float thirdPersonCameraDistance;
+	public float cameraHeight;
+	public float wallCameraBuffer;
+	private bool firstPerson;
+	private float cameraLoc;
+	private float cameraLeft;
+	private float cameraDistance;
 
 	void Start()
 	{
-		cameraMaxHeight = .678f;
-		cameraMinHeight = .5f;
-		cameraMaxDistance = -6f;
-		mouseScroll = cameraMaxDistance;
+		thirdPersonCameraLeft = -.8f;
+		//thirdPersonCameraDistance = -6f;
+		thirdPersonCameraDistance = -9f;
+		cameraHeight = .5f;
+		wallCameraBuffer = .9f;
+		firstPerson = false;
 	}
 
 	void Update()
 	{
+		// MOUSE CLICK ON AN OBJECT
 		RaycastHit hit;
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		float cameraLoc = mouseScroll;
-		// for mouse scroll, when i come away from a surface i want it to go back to what it was before.
-		// however, when i am at a surface i dont want to be scrolling from old mousescroll point,
-		// i want to be scrolling from the wall.
-		// so i need to keep track of old mouse scroll and new mouse scroll separately.
-		// and i need to scale to be squished when next to the wall so the current scroll wheel value is for the wall.
-		mouseScroll += Input.GetAxis("Mouse ScrollWheel")*3;
-		if (Physics.Raycast(transform.parent.transform.position, Input.mousePosition - Camera.main.gameObject.transform.position, out hit, 100.0f))
-		{	
-			if (Input.GetMouseButtonDown(0)){
+		if (Physics.Raycast(ray, out hit, 100.0f))
+		{
+			if (Input.GetMouseButtonDown(0)){ // 0 = left click
 				if (hit.collider)
 				{
+					Debug.Log($"HIT {hit.collider.gameObject.name}");
 					var objectClicks = hit.collider.gameObject.GetComponent<ObjectClicks>();
 					if (objectClicks) {
 						objectClicks.left_click();
@@ -41,20 +42,31 @@ public class CameraScript : MonoBehaviour
 			}
 		}
 
-		// either make camera go up and down when you look up and down so it passes thru the same point on the player always,
-		// or put the camera to the side like fps (maybe this is best)
-		// or make the person a little seethru
-		// or find a way to let raycast see thru a specific object
-		// or take raycast from mouse point to camera, find point on player that intersects, make ray from player point
-		// to mouse point and use that for clicking. should work but, is it even good to have the player in the way of view??
-		float cameraDistance = Vector3.Distance(Camera.main.gameObject.transform.position, transform.parent.transform.position);
-		RaycastHit hit2;
-		if (Physics.Raycast(transform.parent.transform.position, Camera.main.gameObject.transform.position - transform.parent.transform.position, out hit2, cameraDistance))
+		// FIRST PERSON / THIRD PERSON CAMERA LOCATION
+		if (Input.GetKeyDown(KeyCode.Q))
 		{
-			cameraLoc = -Vector3.Distance(hit2.point, transform.parent.transform.position) * .95f;
+			firstPerson = !firstPerson;
 		}
-		cameraLoc = Mathf.Clamp(cameraLoc, cameraMaxDistance, 0f);
-		float cameraHeight = (cameraMinHeight - cameraMaxHeight) / (-cameraMaxDistance) * (cameraLoc - cameraMaxDistance) + cameraMaxHeight;
-		Camera.main.gameObject.transform.localPosition = new Vector3(0, cameraHeight, cameraLoc);
+		if (firstPerson)
+		{
+			cameraLoc = 0;
+			cameraLeft = 0;
+		}
+		else
+		{
+			cameraDistance = Vector3.Distance(transform.position, transform.parent.transform.position);
+			RaycastHit hit2;
+			if (Physics.Raycast(transform.parent.transform.position, transform.position - transform.parent.transform.position, out hit2, cameraDistance * 1.01f / wallCameraBuffer ))
+			{
+				cameraLoc = -Vector3.Distance(hit2.point, transform.parent.transform.position) * wallCameraBuffer;
+			}
+			else
+			{
+				cameraLoc = thirdPersonCameraDistance;
+			}
+			cameraLoc = Mathf.Clamp(cameraLoc, thirdPersonCameraDistance, 0f);
+			cameraLeft = thirdPersonCameraLeft;
+		}
+		Camera.main.gameObject.transform.localPosition = new Vector3(cameraLeft, cameraHeight, cameraLoc);
 	}
 }
--- Player.cs
+++ Player.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Player : MonoBehaviour
-{
-
+public class Player : MonoBehaviour {
 	public float boostStrength;
+	public float jumpStrength;
 	public float movementSensitivity;
 	public float groundedRotationSensitivity;
-	public float floatingRotationSensitivity;
+	public float flyingRotationSensitivity;
 	public float magneticShoesMaxRadius;
+	public float weakMagnetFraction;
+	public float maxMagnetStrength;
+	public float playerMass;
+	public float magnetLockdownStrength;
+	public float magnetLockdownDistance;
+	public GameObject marker;
+	public GameObject arrow;
+	//private GameObject markerCopy;
+	//private GameObject arrowCopy;
 	private float moveHorizontal;
 	private float moveVertical;
 	private float mouseX;
@@ -22,41 +32,80 @@ public class Player : MonoBehaviour
 	private int collisionCount;
 	private Rigidbody rigidBody;
 	private new GameObject camera;
+	private Vector3 elementClosestPointDirection;
 	private Vector3 elementClosestPoint;
-	private float localFootPoint;
+	private float footDist;
+	private Text speedDisplay;
+	private Text magnetShoesDisplay;
+	private float closestElementDistance;
+	private float c1;
+	private float c2;
+	private float magneticStrength;
+	private float tempMagneticStrength;
+	private Vector3 footPoint;
+	// TODO: for slerping
+	//private bool groundedRotateBack;
+	//private float timeCount;
+	//private float slerpSpeed;
 
-	void Start()
-	{
-		rigidBody = this.GetComponent<Rigidbody>();
-		camera = Camera.main.gameObject;
-		Cursor.lockState = CursorLockMode.Locked;
-		boostStrength = 5f;
-		boostKeyPressed = false;
-		movementSensitivity = 5f;
-		groundedRotationSensitivity = .7f;
-		floatingRotationSensitivity = .1f;
-		areMagneticShoesOn = false;
-		rigidBody.maxAngularVelocity = 1f;
-		magneticShoesMaxRadius = 10f;
-		localFootPoint = -transform.lossyScale.y/2;
+	void Start() {
+		rigidBody = this.GetComponent<Rigidbody>(); // set the rigidBody of the player
+		camera = Camera.main.gameObject; // set the gameObject of the camera
+		Cursor.lockState = CursorLockMode.Locked; // lock the cursor into the screen
+		boostStrength = 5f; // boost strength acceleration when flying
+		boostKeyPressed = false; // boost key is pressed flag
+		boostDirection = 1; // boost forward (1) or backward (-1)
+		movementSensitivity = 5f; // keyboard player movement sensitivity when grounded
+		groundedRotationSensitivity = 1f; // player/camera rotation sensitivity when grounded or magnetizing downward
+		flyingRotationSensitivity = 10f; // player rotation torque sensitivity when flying
+		areMagneticShoesOn = false; // magnetic shoes are on flag
+		rigidBody.maxAngularVelocity = 1f; // hard set maximum angular (rotation) velocity of player
+		magneticShoesMaxRadius = 10f; // radius within which magnetic shoes can lock on to an object
+		footDist = -transform.lossyScale.y/2; // distance of foot point from center of player in y direction
+		speedDisplay = GameObject.Find("TextSpeed").GetComponent<Text>(); // speed display text component
+		speedDisplay.text = $"{0.0}"; // speed display text component's string
+		magnetShoesDisplay = GameObject.Find("TextMagnetShoes").GetComponent<Text>(); // magnetic shoe text component
+		magnetShoesDisplay.text = "Magnets OFF"; // magnetic shoe text component's string
+		playerMass = 80f; // player mass. used for player physics
+		rigidBody.mass = playerMass; // hard set player mass
+		weakMagnetFraction = .5f; // magnet shoes initial far (weak) strength fraction compared to max
+		maxMagnetStrength = 7f; // magnet shoes close (strong) strength for normal magnetation
+		jumpStrength = 4f; // strength of jumping (when grounded)
+		magnetLockdownStrength = 10f; // special "lockdown" strength to force sticking still to the surface
+		magnetLockdownDistance = .5f; // distance at which the above magnetLockdownStrength is activated
+		closestElementDistance = magneticShoesMaxRadius * 1.1f; // distance to the closest element to feet
+		// c1, c2 used for physically accurate magnetic force calculation
+		c2 = Mathf.Pow(weakMagnetFraction, .5f) * (Mathf.Pow(weakMagnetFraction, .5f) + 1f) * magneticShoesMaxRadius / (1f - weakMagnetFraction);
+		c1 = Mathf.Pow(c2, 2f) * maxMagnetStrength;
+		magneticStrength = c1 / Mathf.Pow(c2 + closestElementDistance, 2f); // strength of magnetic shoes
+		footPoint = transform.TransformPoint(new Vector3(0, footDist/transform.lossyScale.y, 0)); // point of center of player foot in global coordinates
+		// Objects for displaying points and vectors
+		//markerCopy = Instantiate(marker, new Vector3(0, 0, 0), Quaternion.identity);
+		//markerCopy.SetActive(false);
+		//arrowCopy = Instantiate(arrow, footPoint, Quaternion.identity);
+		//arrowCopy.SetActive(true);
+		// TODO: for slerping
+		//groundedRotateBack = false;
+		//timeCount = 0.0f;
+		//slerpSpeed = .005f;
 	}
 
-	void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.E))
-		{
+	void Update() { // updates with time (frames)
+		// Use this to output something to the log
+		//Debug.Log("Update");
+
+		// GET KEYS PRESSED AND MOUSE MOVEMENTS
+		if (Input.GetKeyDown(KeyCode.E)) { // stop player (only for testing)
 			rigidBody.velocity = new Vector3(0, 0, 0);
 		}
-		if (Input.GetKey("space"))
-		{
+		if (Input.GetKey("space")) { // boosting
 			boostKeyPressed = true;
 			boostDirection = Input.GetKey("left shift") ? -1 : 1;
 		}
-		if (Input.GetKeyDown(KeyCode.F))
-		{
+		if (Input.GetKeyDown(KeyCode.F)) { // magnetic shoes on / off
 			areMagneticShoesOn = !areMagneticShoesOn;
-			if (!areMagneticShoesOn)
-			{
+			magnetShoesDisplay.text = areMagneticShoesOn ? "Magnets ON" : "Magnets OFF";
+			if (!areMagneticShoesOn) {
 				unGround();
 			}
 		}
@@ -65,134 +114,154 @@ public class Player : MonoBehaviour
 		mouseX = Input.GetAxis("Mouse X");
 		mouseY = Input.GetAxis("Mouse Y");
 
+		// TODO: for slerping
+		//timeCount = timeCount + Time.deltaTime;
+
 		// PLAYER ROTATION
-		if (areMagneticShoesOn & isGrounded)
-		{ // GROUNDED PLAYER ROTATION
-			rigidBody.transform.Rotate(new Vector3(0, mouseX, 0) * groundedRotationSensitivity);
+		footPoint = transform.TransformPoint(new Vector3(0, footDist/transform.lossyScale.y, 0));
+		if (areMagneticShoesOn & isGrounded) {
+			// GROUNDED PLAYER ROTATION
+			rigidBody.transform.Rotate(new Vector3(0, mouseX, 0) * groundedRotationSensitivity); // body rotates only Y
+			// rotate the camera in X direction instead of the body because body is fixed to ground
 			camera.transform.Rotate(new Vector3(-mouseY, 0, 0) * groundedRotationSensitivity);
-		}
-		else
-		{ // FLYING PLAYER RORATION, REGARDLESS OF MAGNET SHOES ON OR NOT
-			rigidBody.AddRelativeTorque(new Vector3(-mouseY, mouseX, 0) * floatingRotationSensitivity);
-		}
-	}
-
-	void FixedUpdate()
-	{
-		float velX;
-		float velY;
-		float velZ;
-		Vector3 relVel = transform.InverseTransformDirection(rigidBody.velocity);
-		float magneticStrength = 5;
-		Vector3 footPoint = transform.TransformPoint(new Vector3(0, localFootPoint/transform.lossyScale.y, 0));
-
-		// PLAYER MOVEMENT
-		if (areMagneticShoesOn)
-		{ // MAGNET SHOES ON PLAYER MOVEMENT
+		} else if (areMagneticShoesOn) { // magnet shoes on, but not yet grounded
+			// MAGNETS ON BUT NOT GROUNDED YET
+			// get closest element
 			RaycastHit[] hits = Physics.SphereCastAll(transform.position, magneticShoesMaxRadius, transform.forward, 0);
 			Collider closestElement = null;
-			int doOrNot = 0;
-			float closestElementDistance = 1000f * magneticShoesMaxRadius;
-			for (int i = 0; i < hits.Length; i++)
-			{
-				Vector3 tempElementClosestPoint = hits[i].collider.ClosestPointOnBounds(footPoint);
-				float elementDistance = Vector3.Distance(tempElementClosestPoint, footPoint);
-				if (hits.Length > 1) // player is always closest element, so ignore that first element
-				{
-					if (elementDistance < closestElementDistance & hits[i].collider.transform.gameObject.name != "Player")
-					{
-						closestElement = hits[i].collider;
-						elementClosestPoint = tempElementClosestPoint - footPoint;
-						isGrounded = elementClosestPoint == new Vector3(0, 0, 0);
-						closestElementDistance = elementDistance;
-						doOrNot = 1;
-					}
+			closestElementDistance = magneticShoesMaxRadius;
+			int isMagnetNear = hits.Length > 1 ? 1 : 0; // goes into magneticStrength as factor of 1 or 0 if magnet is or isn't near 
+			if (Convert.ToBoolean(isMagnetNear)) { // element exists (player is always closest element, so ignore that first element)
+				for (int i = 0; i < hits.Length; i++) { // search for closest element
+					Vector3 tempElementClosestPoint = hits[i].collider.ClosestPointOnBounds(footPoint);
+					float elementDistance = Vector3.Distance(tempElementClosestPoint, footPoint);
+						if (elementDistance < closestElementDistance & hits[i].collider.transform.gameObject.name != "Player") {
+							// element is closer than any other closer element we've checked so far
+							closestElement = hits[i].collider;
+							elementClosestPoint = tempElementClosestPoint;
+							closestElementDistance = elementDistance;
+						}
 				}
+				elementClosestPointDirection = (elementClosestPoint - footPoint).normalized;
+				isGrounded = elementClosestPointDirection == new Vector3(0, 0, 0);
+				// TODO: for slerping
+				//if (isGrounded) {
+				//	// this will start rotation back to forward after looking toward the magnet spot
+				//	groundedRotateBack = true;
+				//}
 			}
-			if (!isGrounded)
-			{ // MAGNETIZING DOWNWARD PLAYER MOVEMENT, STILL NOT GROUNDED
-				// get ray cast from feet to surface below feet, use that distance as F = c2/(c1+d)^2
-				// once i can get the distance between player and closest thing below it, i can see the force of the magnet
-				// and if theres nothing below it, the for
-				// if i had feet be separate, i think i would cause a torque toward the surface which would be more realistic, want to do that at the end
-				rigidBody.AddForceAtPosition(elementClosestPoint.normalized * magneticStrength * doOrNot, footPoint, ForceMode.Acceleration);
+			magneticStrength = c1 / Mathf.Pow(c2 + closestElementDistance, 2f) * isMagnetNear;
 
-				
-				if (boostKeyPressed)
-				{
+			// if there is a magnet locked on
+			if (Convert.ToBoolean(isMagnetNear)) {	// mouse controls camera rotation
+				camera.transform.Rotate(new Vector3(-mouseY, mouseX, 0) * groundedRotationSensitivity);
+			} else { // otherwise if not locked on magnet
+				// mouse controls body rotation
+				rigidBody.AddRelativeTorque(new Vector3(-mouseY, mouseX, 0) * flyingRotationSensitivity); // note: mouse controls seem to work best in Update
+			}
+		} else {
+			// FLYING PLAYER ROTATION, MAGNET SHOES OFF
+			// mouse controls body rotation
+			rigidBody.AddRelativeTorque(new Vector3(-mouseY, mouseX, 0) * flyingRotationSensitivity); // note: mouse controls seem to work best in Update
+			// while flying, camera is to be pointing forward. we slerp up to it so that it doesnt move there suddenly after taking off
+			// TODO: make it slerp instead of instantly looking
+			//camera.transform.localRotation = Quaternion.Slerp(camera.transform.localRotation, Quaternion.Euler(0, 0, 0), timeCount * slerpSpeed);
+			camera.transform.localRotation = Quaternion.Euler(0, 0, 0);
+		}
+
+		// DISPLAY TEXT
+		speedDisplay.text = $"Speed {Mathf.Round(rigidBody.velocity.magnitude * 10f) / 10f}";
+	}
+
+	void FixedUpdate() { // updates with physics
+		float velX = 0; // will be used at the end of this to set the X velocity of the player in relative coordinates
+		float velY = 0; // will be used at the end of this to set the Y velocity of the player in relative coordinates
+		float velZ = 0; // will be used at the end of this to set the Z velocity of the player in relative coordinates
+		Vector3 relVel = transform.InverseTransformDirection(rigidBody.velocity); // current relative velocity of player
+		// PLAYER MOVEMENT (NOT ROTATION)
+		if (areMagneticShoesOn) {
+		// MAGNET SHOES ON PLAYER MOVEMENT
+			if (!isGrounded) {
+			// MAGNETIZING DOWNWARD PLAYER MOVEMENT, STILL NOT GROUNDED
+				rigidBody.constraints = RigidbodyConstraints.None;
+				// special "lockdown" strength to force sticking still to the surface
+				float lockDownFactor = closestElementDistance <= magnetLockdownDistance & closestElementDistance != 0 ? magnetLockdownStrength : 1;
+				float ang = Vector3.Angle(-transform.up, elementClosestPointDirection);
+				bool layingDownCheck = closestElementDistance <= .6f & closestElementDistance >= .1f & !isGrounded &  ang >= 40f & ang <= 95f;
+				if (layingDownCheck) {
+					Debug.Log("DID IT");
+					rigidBody.AddTorque(Vector3.Cross(-transform.up, elementClosestPointDirection).normalized * 100f);
+				} else {
+					Debug.Log("DID NOT DO IT");
+					// magnetic force toward footPoint (if magnet is not near the strength is 0)
+					rigidBody.AddForceAtPosition(elementClosestPointDirection * magneticStrength * lockDownFactor, footPoint, ForceMode.Acceleration);
+				}
+				// still allow boosting while not totally grounded
+				if (boostKeyPressed) { // boosting
 					rigidBody.AddRelativeForce(Vector3.forward * boostStrength * boostDirection, ForceMode.Acceleration);
-					if (!Input.GetKey("space"))
-					{
+					if (!Input.GetKey("space")) {
 						boostKeyPressed = false;
 					}
 				}	
-				velX = relVel.x;
-				velY = relVel.y;
-				velZ = relVel.z;
-				
-				rigidBody.constraints = RigidbodyConstraints.None;
-			}
-			else
-			{ // GROUNDED PLAYER MOVEMENT
-				if (stickIt)
-				{
+				velX = relVel.x; // velX not controled, changes naturally based on phyisics from external forces
+				velY = relVel.y; // velY not controled, changes naturally based on phyisics from external forces
+				velZ = relVel.z; // velZ not controled, changes naturally based on phyisics from external forces
+			} else {
+			 	// GROUNDED PLAYER MOVEMENT
+				rigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+				if (stickIt) {
 					rigidBody.angularVelocity = new Vector3(0, 0, 0);
 					rigidBody.velocity = new Vector3(0, 0, 0);
+					camera.transform.localRotation = Quaternion.Euler(0, 0, 0);
 					stickIt = false;
 				}
-				rigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+				if (boostKeyPressed) { // JUMP UP WHEN GROUNDED (INSTEAD OF BOOST FORWARD) BTW THEN YOU'RE NOT GROUNDED
+					rigidBody.AddRelativeForce(Vector3.up * jumpStrength, ForceMode.VelocityChange);
+					if (!Input.GetKey("space")) {
+						boostKeyPressed = false;
+					}
+				}
 				rigidBody.AddRelativeForce(Vector3.down * magneticStrength, ForceMode.Acceleration);
-				velX = moveHorizontal;
-				velY = relVel.y;
-				velZ = moveVertical;
+				velX = moveHorizontal; // velX controled by keyboard
+				velY = relVel.y; // velY not controled, changes naturally based on phyisics from external forces
+				velZ = moveVertical; // velZ controled by keyboard
 			}
-		}
-		else
-		{ // FLYING PLAYER MOVEMENT WITH MAGNET SHOES OFF
-			if (boostKeyPressed)
-			{
+		} else {
+			// FLYING PLAYER MOVEMENT WITH MAGNET SHOES OFF
+			if (boostKeyPressed) { // boosting
 				rigidBody.AddRelativeForce(Vector3.forward * boostStrength * boostDirection, ForceMode.Acceleration);
-				if (!Input.GetKey("space"))
-				{
+				if (!Input.GetKey("space")) {
 					boostKeyPressed = false;
 				}
 			}
-			
 			rigidBody.constraints = RigidbodyConstraints.None;
-			velX = relVel.x;
-			velY = relVel.y;
-			velZ = relVel.z;
+			velX = relVel.x; // velX not controled, changes naturally based on phyisics from external forces
+			velY = relVel.y; // velY not controled, changes naturally based on phyisics from external forces
+			velZ = relVel.z; // velZ not controled, changes naturally based on phyisics from external forces
 		}
-
+		// translate relative velocity to global velocity and set it
 		rigidBody.velocity = transform.rotation * new Vector3(velX, velY, velZ);
 	}
 
 	// DETECT COLLISION WITH AN OBJECT
-	void OnCollisionEnter(Collision hit)
-	{
+	void OnCollisionEnter(Collision hit) {
 		collisionCount += 1;
-		if (!isGrounded & areMagneticShoesOn)
-		{ // if you hit a wall while already walking on the ground, don't do anything. only do something if not grounded and you touch a surface
-			// also don't do anything if you touch a surface and your magnets arent on
+		if (!isGrounded & areMagneticShoesOn) {
 			stickIt = true;
 		}
 	}
  
 	// DETECT EXITING A COLLOSION WITH AN OBJECT
-	void OnCollisionExit(Collision hit)
-	{
-		if (collisionCount == 1)
-		{ // only become ungrounded if the surface you stopped touching was the ground
+	void OnCollisionExit(Collision hit) {
+		if (collisionCount > 0) { // don't decrease collisionCount into the negatives
+			collisionCount -= 1;
+		}
+		if (collisionCount == 0) { // only become ungrounded if the surface you stopped touching was the last collision
 			unGround();
 		}
 	}
 
-	void unGround()
-	{
-		if (collisionCount > 0)
-		{
-			collisionCount -= 1;
-		}
+	void unGround() {
 		isGrounded = false;
 		stickIt = false;
 	}
